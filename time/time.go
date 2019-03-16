629419c70f0bb4993711c36964451be7bb1ba9cc
bkawk <email.will.in.china@gmail.com>
2019-03-16 23:35:14 +0800

    time

---
+++ time/time.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func count(id int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(id, ":", i)
+		time.Sleep(time.Millisecond * 1000)
+	}
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go count(i)
+	}
+	time.Sleep(time.Millisecond * 20000)
+}
+
+// go run time.go
